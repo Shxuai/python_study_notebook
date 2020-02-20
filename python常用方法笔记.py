10a9c0cdfc4456c21d053c2f391300223a1eb2ee
Shxuai <769804622@qq.com>
2020-02-20 18:02:21 +0800

    no message

--- python常用方法笔记.py
+++ python常用方法笔记.py
@@ -1,4 +1,5 @@
 # np.array多维数组降维
+# 只能删除维度为1的维度
 a = np.array([[1], [2]])
 b = np.squeeze(a)
 print(a, b)
@@ -32,3 +33,18 @@ for x in np.nditer(c):
 # 将给定十进制int变量i编码为位数为num_digits的二进制数组,不足的位数补0
 def binary_encode(i, num_digits):
     return np.array([i >> d & 1 for d in range(num_digits)][::-1])
+
+
+# 将矩阵变为另一任意维度矩阵
+# 再在元素任意位置插入某矩阵
+a = np.random.rand(2, 3, 2)
+# 首先将矩阵用flatten方法降维到一维
+a.flatten()
+print(a)
+# 然后用resize任意变化
+a.resize(2, 3)
+print(a)
+# 在矩阵末尾插入一个[1,1]的数组,注意此处数组len必须与矩阵的操作维度上一维度列数相等
+# 也就是说, values引用的一维数组len必须等于(axis-1)维度的len
+a = np.insert(a, 3, values=np.ones(2), axis=1)
+print(a)
