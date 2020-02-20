028bf5a524b2f9c1bee6d2f6693524a3be8143a3
Shxuai <769804622@qq.com>
2020-02-20 09:59:02 +0800

    no message

--- python常用方法笔记.py
+++ python常用方法笔记.py
@@ -7,23 +7,28 @@ print(a, b)
 # 可通过order控制遍历优先顺序
 # for x in np.nditer(a, order='F'):Fortran order，即是列序优先；
 # for x in np.nditer(a.T, order='C'):C order，即是行序优先；
-a = np.arange(0,60,5)
-a = a.reshape(3,4)
-print ('原始数组是：')
-print (a)
-print ('\n')
-print ('原始数组的转置是：')
+a = np.arange(0, 60, 5)
+a = a.reshape(3, 4)
+print('原始数组是：')
+print(a)
+print('\n')
+print('原始数组的转置是：')
 b = a.T
-print (b)
-print ('\n')
-print ('以 C 风格顺序排序：')
+print(b)
+print('\n')
+print('以 C 风格顺序排序：')
 c = b.copy(order='C')
-print (c)
+print(c)
 for x in np.nditer(c):
-    print (x, end=", " )
-print  ('\n')
-print  ('以 F 风格顺序排序：')
+    print(x, end=", ")
+print('\n')
+print('以 F 风格顺序排序：')
 c = b.copy(order='F')
-print (c)
+print(c)
 for x in np.nditer(c):
-    print (x, end=", " )
+    print(x, end=", ")
+
+
+# 将给定十进制int变量i编码为位数为num_digits的二进制数组,不足的位数补0
+def binary_encode(i, num_digits):
+    return np.array([i >> d & 1 for d in range(num_digits)][::-1])
